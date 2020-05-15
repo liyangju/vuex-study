dcb318a2883c370a3653a71f1172222eafa72bc5
liyangju <869425299@qq.com>
2020-05-15 14:31:27 +0800

    first commit

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,40 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    count: 1,
+    num: 666
+  },
+  getters: {
+    getState: state => {
+      return state.count + 100
+    }
   },
   mutations: {
+    add: (state, n) => state.count = state.count + n,
+    red: (state, n) => state.count = state.count - n,
+    number: (state, n) => state.num += n
   },
   actions: {
+    addFn({
+      commit
+    }, n) {
+      commit("add", n)
+    },
+    redFn({
+      commit
+    }, n) {
+      commit("red", n)
+    },
+    numberFn({
+      commit
+    }, n) {
+      return new Promise((reslove) => {
+        setTimeout(() => {
+          commit("number", n)
+          reslove()
+        }, 2000)
+      })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{this.$store.state.count}}</h1>
+    <h2>{{this.getState}}</h2>
+    <button @click="addFn">+</button>
+    <button @click="redFn(8)">-</button>
+    <button @click="numberFn">{{this.$store.state.num}}</button>
   </div>
 </template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["getState"])
+  },
+  mounted() {
+    console.log(this.$store.state.count);
+    console.log(this.$store.getters.getState);
+    console.log(this.$store);
+  },
+  methods: {
+    addFn() {
+      this.$store.dispatch("addFn", 10);
+    },
+
+    ...mapActions(["redFn"]),
+
+    numberFn() {
+      this.$store.dispatch("numberFn", 111).then(() => {
+        alert(1233);
+      });
+    }
+  }
+};
+</script>
